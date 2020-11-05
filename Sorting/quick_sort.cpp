fb04c2ce5909fc939b0dca17f7cc7873eaa64aa7
RichardTimothy1307 <richardtimothy82@gmail.com>
2020-11-05 23:18:29 +0530

    Code for quick sort

---
+++ Sorting/quick_sort.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//TODO:To implement Quicksort
+//*Quicksort is an in place sorting Algorithm 
+//*Takes a pivot and makes the array such that all greater than it comes to right of it and smaller to left
+//*Recursively (Divide and Conquer)
+
+
+//*Partition Function to get the index and the pivot at the correct position
+
+
+int Partition(int* a,int start,int end){
+
+        int pivot=a[end];  //*The pivot assigned as the last element
+        int pindex=start;  //*The index for pivot
+        int j;
+        for(j=start;j<end;j++){
+            if(a[j]<pivot){
+                int temp=a[j];
+                a[j]=a[pindex];
+                a[pindex]=temp;
+                pindex++;
+            }
+
+        }
+        int temp=a[pindex];
+        a[pindex]=a[end];
+        a[end]=temp;
+
+        return pindex;
+
+
+}
+
+
+
+//*QuickSort function which is halve itself and gets called recursively
+
+void QuickSort(int* a,int start,int end){
+    if(start>=end){
+        return;  //*base case
+    }
+
+    int PartitionIndex= Partition(a,start,end);   //*gets the the correct partion index 
+    QuickSort(a,start,PartitionIndex-1);          //*Left Half
+    QuickSort(a,PartitionIndex+1,end);            //*Right Half
+
+
+
+}
+
+
+
+int main(){
+
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++){
+       cin>>a[i];
+   }
+
+    QuickSort(a,0,n-1);
+
+
+
+   for(auto v:a){
+       cout<<v<<" ";
+   }
+
+
+
+
+    return 0;
+}
